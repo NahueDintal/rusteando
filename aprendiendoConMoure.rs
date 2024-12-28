21ee022836d40612bd679e405ee3dc743916f41f
nahuedintal <nahue.dintal@gmail.com>
2024-12-28 18:34:15 -0300

    usando el archivo por defecto main.rs para aprender rust

--- aprendiendoConMoure.rs
+++ aprendiendoConMoure.rs
@@ -2,4 +2,13 @@
 
 fn main() {
     println!("Hola Rustito!!");
+
+//Comentario 
+/* aca otro comentario
+ y acá más comentario */
+
+// Variables
+
+let mi-string: &str = "Esto es una cadena de testo";
+println!("Variable: {}", mi-string);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Acá empezando con el Rustito!
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hola Rustito!!");
+
+//Comentario 
+/* aca otro comentario
+ y acá más comentario */
+
+// Variables
+
+let mi-string: &str = "Esto es una cadena de testo";
+println!("Variable: {}", mi-string);
 }
