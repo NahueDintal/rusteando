ff95736db25066c7b7003d6f6d41ca76d150f37f
nahuedintal <nahue.dintal@gmail.com>
2025-01-07 17:03:21 -0300

    haciendo el ejercicio2

--- ejercicio2/src/main.rs
+++ ejercicio2/src/main.rs
@@ -1,18 +1,3 @@
 use std::io;
 
-// Calculadora simple 
-fn main () {
-// Pregunte que operacion realizar
-    println!("Que operacion desea realizar?");
 
-
-// Bienvenida y que pida 2 numeros.
-    println!("Buenvenido a esta calculadora simple, ingrese 2 numeros.");
-
-    let mut numeros = num::new();
-
-// Muestre el resultado
-
-    println!("Tu resultado es {}", result);
-
-}
