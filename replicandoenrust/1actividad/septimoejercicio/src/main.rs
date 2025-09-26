9f61f014a2923c0fee4cae509287f4263b63fcc4
nahuedintal <nack3rz@proton.me>
2025-09-26 15:18:42 -0300

    sigo con un nuevo ejercicio de simulador de un cajero

---
+++ replicandoenrust/1actividad/septimoejercicio/src/main.rs
@@ -0,0 +1,13 @@
+// Simulación de un cajero
+// ingresar por consola el usuario y la clave
+// user , 1234
+// si el logeo es correcto dar bienvenida al sistema
+// caso contrario, logeo incorrecto
+// una vez ingresado el usuario tiene 500k
+// opcion 1 deposito van a ingresar el monto a ingresar
+// opcion 2 transferencia, solo puede transferir lo que tiene
+// en ambos casos imprimir los montos de la operacion
+// Entradas
+fn main() {
+    println!("Hello, world!");
+}
