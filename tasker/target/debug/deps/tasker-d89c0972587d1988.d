8c6f3a96139a63196a44575d3d749c081ac098ea
nahuel dintal <nahue.dintal@gmail.com>
2025-11-02 02:10:37 -0300

    terminado ejercicios

--- tasker/target/debug/deps/tasker-d89c0972587d1988.d
+++
@@ -1,5 +0,0 @@
-/home/nahue/rusteando/tasker/target/debug/deps/tasker-d89c0972587d1988.d: src/main.rs
-
-/home/nahue/rusteando/tasker/target/debug/deps/tasker-d89c0972587d1988: src/main.rs
-
-src/main.rs:
--- tasker/target/debug/tasker.d
+++
@@ -1,1 +0,0 @@
-/home/nahue/rusteando/tasker/target/debug/tasker: /home/nahue/rusteando/tasker/src/main.rs
